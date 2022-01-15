0ffe491947eab09965822acdf7d45ad0209a5452
Michael Curry <michaelcurry95@gmail.com>
2022-01-15 12:04:36 -0500

    initial layout...

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,41 @@
+<div class="wrapper">
+	<header>
+		<h1>Pet A Cat</h1>
+		<p>A delightful library of cat pictures you can pet!</p>
+		<a href="http://twitter.com/mikedubcurry">@mikedubcurry</a>
+	</header>
+
+	<main><slot /></main>
+</div>
+
+<style>
+	:global(html, body, #svelte) {
+		height: 100%;
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
+
+	.wrapper {
+		min-height: 100%;
+		display: grid;
+		grid-template-columns: 38.22% 61.77%;
+	}
+
+	header {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		height: 38.22%;
+		align-self: center;
+	}
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
